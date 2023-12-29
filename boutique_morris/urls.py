df507806f6a12a91c9c1d4f772de0bc68ffe4d64
michael santana <127775871+micsanhag@users.noreply.github.com>
2023-12-29 15:39:38 +0000

    update

--- boutique_morris/urls.py
+++ boutique_morris/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import Settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(Settings.MEDIA_URL, document_root=Settings.MEDIA_ROOT)
