faca42957ba7594b8ccf35bfea82fad6e7926e98
michael santana <127775871+micsanhag@users.noreply.github.com>
2024-01-03 10:55:18 +0000

    update

--- products/views.py
+++ products/views.py
@@ -4,24 +4,24 @@ from .models import Product
 # Create your views here.
 
 def all_products(request):
-    """A view to show all products, including sorting and search queries."""
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
     context = {
-        'products': products
+        'products': products,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    """A view to show individual product details."""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
